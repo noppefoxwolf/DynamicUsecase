2161ceb372325396f0171d0ffd1ff2d6c180ab32
tomoya.hirano <tomoya.hirano@dena.com>
2020-12-10 01:14:55 +0900

    init

---
+++ FrameworkA/FrameworkA.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkA.h
+//  FrameworkA
+//
+//  Created by Tomoya Hirano on 2020/12/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkA.
+FOUNDATION_EXPORT double FrameworkAVersionNumber;
+
+//! Project version string for FrameworkA.
+FOUNDATION_EXPORT const unsigned char FrameworkAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkA/PublicHeader.h>
+
+
