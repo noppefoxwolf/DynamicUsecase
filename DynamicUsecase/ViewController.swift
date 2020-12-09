06ac1c9cb224b4460e63091c20182c3a80d9ccc3
tomoya.hirano <tomoya.hirano@dena.com>
2020-12-10 01:06:26 +0900

    Initial Commit

---
+++ DynamicUsecase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicUsecase
+//
+//  Created by Tomoya Hirano on 2020/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
